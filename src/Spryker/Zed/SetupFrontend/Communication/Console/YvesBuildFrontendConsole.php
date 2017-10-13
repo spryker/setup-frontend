be1714cd7c2c644f0f5b7400b665628b22b8aed0
René Klatt <rene.klatt@spryker.com>
2017-10-13 13:25:58 +0000

    core-2001 review changes

---
+++ src/Spryker/Zed/SetupFrontend/Communication/Console/YvesBuildFrontendConsole.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\SetupFrontend\Communication\Console;
+
+use Spryker\Zed\Kernel\Communication\Console\Console;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+
+class YvesBuildFrontendConsole extends Console
+{
+    const COMMAND_NAME = 'frontend:yves-build-frontend';
+    const DESCRIPTION = 'This command will build Yves frontend.';
+
+    /**
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->setName(self::COMMAND_NAME);
+        $this->setDescription(self::DESCRIPTION);
+
+        parent::configure();
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     *
+     * @return int|null
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->info('Build Yves frontend');
+
+        $process = new Process('npm run yves', APPLICATION_ROOT_DIR);
+        $process->run(function ($type, $buffer) {
+            echo $buffer;
+        });
+
+        return static::CODE_SUCCESS;
+    }
+}
